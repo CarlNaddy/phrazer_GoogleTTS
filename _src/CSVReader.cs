ae10a9d0d7cdc617b780713d133977f66b00ca81
Kyrylo Nadobnykh <knadobnykh@symbilitysolutions.com>
2019-11-29 18:04:10 +0100

    FIXES: filename, skip unneded row and more

--- _src/CSVReader.cs
+++ _src/CSVReader.cs
@@ -5,7 +5,7 @@ using System.IO;
 namespace Phrazer
 {
     class CSVReader
-    {   
+    {
         public string InputFileName { get; set; }
         public int RowNumberCsv { get; set; }
         public int RowNumberTpl { get; set; }
@@ -24,30 +24,42 @@ namespace Phrazer
         public string GetOutputFilename()
         {
             // Generate a file from the script and save
-            string OutputSortOrderStr = ("" + (RowNumberCsv-1)).PadLeft(3, '0');
-            return GTTSAppdata.GetExportPath(InputFileName) + OutputSortOrderStr + "." + FromLang + "-" + ToLang + "." + FromText + "-" + ToText + "." + "wav";
+            string OutputSortOrderStr = ("" + (RowNumberCsv - 1)).PadLeft(3, '0');
+            string OFileName = AdjustPath(OutputSortOrderStr + "." + FromLang + "-" + ToLang + "." + FromText + "-" + ToText + "." + "wav");
+            return GTTSAppdata.GetExportPath(InputFileName) + OFileName;
+        }
+
+        private string AdjustPath(string Input)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(Input, @"[\\/:*?""<>|]", string.Empty);
         }
 
         public void ProceedRow(string csvRow)
         {
-            string[] csvEntries = csvRow.Split('\t');
-            if(csvEntries.Length < 2) return; // Skip not well defined row /placeholder
+            string[] csvEntries = csvRow.Trim().Split('\t');
 
-            if(RowNumberCsv == 1) {
+            // File.WriteAllText(GetOutputFilename() + ".txt", csvRow.Trim());
+            // return;
+
+            if (csvEntries.Length < 2) return; // Skip not well defined row /placeholder
+
+            if (RowNumberCsv == 1)
+            {
                 FromLang = csvEntries[0];
                 ToLang = csvEntries[1];
                 return;
             }
 
             FromText = csvEntries[0];
-            ToText   = csvEntries[1];
+            ToText = csvEntries[1];
 
             ProcessTplFile();
         }
 
         public void ProceedCsvFile(string currentFileName)
         {
-            if(!File.Exists(currentFileName)) {
+            if (!File.Exists(currentFileName))
+            {
                 Console.WriteLine("File " + currentFileName + " not exists!");
                 return;
             }
@@ -55,7 +67,8 @@ namespace Phrazer
             InputFileName = currentFileName;
             string[] rows = File.ReadAllLines(InputFileName);
             RowNumberCsv = 0;
-            foreach(string csvString in rows) {
+            foreach (string csvString in rows)
+            {
                 RowNumberCsv++;
                 ProceedRow(csvString);
             }
@@ -70,7 +83,8 @@ namespace Phrazer
             Generator = new GTTSGenerator();
             string[] rows = File.ReadAllLines(GTTSAppdata.GetTplPath(ToText));
             RowNumberTpl = 0;
-            foreach(string row in rows) {
+            foreach (string row in rows)
+            {
                 RowNumberTpl++;
                 ProcessTplRow(row);
             }
@@ -93,13 +107,13 @@ namespace Phrazer
 
             // Than SPLIT
             string[] csvEntries = text.Split(':');
-            if(csvEntries.Length < 2) return;
+            if (csvEntries.Length < 2) return;
 
             CurrentVoice = csvEntries[0].Trim();
-            CurrentSsml   = csvEntries[1].Trim();
+            CurrentSsml = csvEntries[1].Trim();
 
             // Please call before going to the G-TTS
-            if(File.Exists(GetOutputFilename())) return;
+            if (File.Exists(GetOutputFilename())) return;
 
             Generator.SynthesizeSSML(CurrentVoice, CurrentSsml);
         }
